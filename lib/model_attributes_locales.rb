f797499945e4f2456e364a578215041ee809ef3f
joaoribeirost <joaoribeirost@gmail.com>
2016-01-12 15:28:58 +0000

    Added Generate Method to terminal

--- lib/model_attributes_locales.rb
+++ lib/model_attributes_locales.rb
@@ -4,11 +4,20 @@ module ModelAttributesLocales
   # Your code goes here...
   require 'model_attributes_locales/railtie' if defined? ::Rails::Railtie
 
-  def generate
-    puts "Gen"
+  def self.generate
+    text += '\t activerecord:'
+    ActiveRecord::Base.connection.tables.map do |model|
+      begin
+        m = model.classify.constantize
+        text += '\t\t' + model.singularize + ":"
+        m.column_names.each do |name|
+          text += '\t\t\t' + name + ":"
+        end
+      rescue
+        #puts "No Class"
+      end
+      puts text
+    end
   end
 
-  def self.hello
-    puts "hi there"
-  end
 end
