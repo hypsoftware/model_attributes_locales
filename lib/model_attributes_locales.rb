55d555cf92bd8d08abaa48d49f1a6fc341945bec
joaoribeirost <joaoribeirost@gmail.com>
2016-01-12 16:07:32 +0000

    Fixing file writing

--- lib/model_attributes_locales.rb
+++ lib/model_attributes_locales.rb
@@ -1,17 +1,20 @@
 require "model_attributes_locales/version"
+require 'yaml'
 
 module ModelAttributesLocales
   # Your code goes here...
+
   require 'model_attributes_locales/railtie' if defined? ::Rails::Railtie
 
   def self.generate
-    text = '\t activerecord:'
+
+    text = '/\t activerecord:'
     ActiveRecord::Base.connection.tables.map do |model|
       begin
         m = model.classify.constantize
-        text += '\t\t' + model.singularize + ":"
+        text += '/\t/\t' + model.singularize + ":"
         m.column_names.each do |name|
-          text += '\t\t\t' + name + ":"
+          text += '/\t/\t/\t' + name + ":"
         end
 
       rescue
@@ -20,11 +23,10 @@ module ModelAttributesLocales
       puts "Next Model"
     end
 
+
     target = "#{Rails.root}/config/locales/model_attributes_locales.yml}"
-    puts "File Written: "
-    File.open(target, "w") do |f|
-      f.write(text)
-    end
+    File.open(target, 'w') {|f| f.write d.to_yaml } #Store
+
   end
 
 end
